121e106188f2a80d2e66345081ac89b571cc2b2f
Ármann Hallbert Jónsson <armann.hj@outlook.com>
2017-05-06 11:06:02 +0000

    Verkefni 4

---
+++ Verkefni_4/index.php
@@ -0,0 +1,150 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylesheets/stylesheet.css">
+        <link rel="stylesheet" href="stylesheets/normalize.css">
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+			<link rel="stylesheet" href="icons/style.css">
+        <title>Git Hub ráðstefna - Verkefni 4</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body>
+			<header>
+      	<div>
+					<input class="input" type="checkbox" id="toggle">
+        	<label class="checkbox" for="toggle">&#9776;  Verkefni 4</label>
+				<nav class="menu">
+					<ul class="topmenu">
+						<a href="#i1"><li>Ræðumenn</li></a>
+						<a href="#i2"><li>Markmið okkar!</li></a>
+						<a href="#i3"><li>Salir</li></a>
+						<a href="#i4"><li>Hvar verður þetta?</li></a>
+					</ul>
+				</nav>
+				</div>
+      </header>
+			
+				<div class="back-img img1"></div>
+        <div id="i1" class="page">
+					<div class="content">
+						<h1><div class="icon-home"></div>Helstu ræðumenn okkar.</h1>
+						<div class="artists">
+							<div class="col-1-3"><h2>Tom Preston-Werner</h2><p>Tom Prestion-Werner, stofandi og fyrrverandi forstjóri Github mun byrja þessa ráðstefnu með því að tala hverni GitHub byrjaði, hverni var að stofna fyrir tækið og sjá það vaxa. Hann mun fara yfir hvert hann vil að GitHub stefni á næstum árum og hvaða við getum fengið að sjá á næstum árum.</p></P></div>
+							<div class="col-1-3"><h2>Chris Wanstrah</h2><p>Chris Wanstrath er co founder og nuverandi forstjóri GitHubs. Hann mun hafa sýnikennslu á því nýjasta sem er komið á Github. Vil hann endilega að flestir koma með tölvu svo fólk getur tekið þátt í því með honum. Ráðlagt að hlaða tölvur áður en er komið.</p></div>
+							<div class="col-1-3"><h2>PY Hyett</h2><p>PY Hyett kemur líka og verður með stutt byrjaenda námskeið fyrir þá sem vilja byrja að nota GitHub. Gott fyrir að fyrirtæki að senda fólk sitt á þetta.</p></div>
+						</div>
+          
+          <div class="artists">
+							<div class="col-1-3"><h2>Nikolaus Diodotos Schwangau</h2><p>Nikolaus Diodotos Schwangau fer yfir sögu githubs og með spennandi leiki.</p></div>
+							<div class="col-1-3"><h2>Rameses Teodor Leitzke</h2><p>Rameses Teodor Leitzke heldur námskeið um að hverni eigi að nýta Github í hópaverkefnum.</p></div>
+							<div class="col-1-3"><h2>Alaric Reino Eldred</h2><p>Alaric Reino Eldred verður live kennslu á github.</p></div>
+						</div>
+					</div>
+					</div>
+					
+        
+			
+			
+				<div class="back-img img2"></div>
+        <div id="i2" class="page">
+					<div class="content">
+						<h1><div class="icon-man-woman"></div>Þetta verður það besta fyrir GitHub fólk á Íslandi</h1>
+
+						<p>Þessi ráðstefna verður sú besta fyrir alla sem nota github eða vilja læra á githubd fyrir vinnu eða hvað sem er.</p>
+						<p>Íslenskt áhugafólk um forritun ætti ekki að leyfa þessu tækifæri að fara framhjá sér.</p>
+						<p>Ef vel gengur mun við ná að draga til okkur fleiri nöfn fyrir fleiri ráðstefnur.</p>
+						<p>Svo endilega segja öllum frá þessu og mæta til okkar daginn 07.07.2017 í þessa rugluð github veislu sem verður hér í hörpuni!</p>
+
+					</div>
+					
+					
+					
+				<div class="back-img img3"></div>
+        <div id="i3" class="page">
+        	<div class="content">
+						<h1><div class="icon-calendar"></div>Venues</h1>
+            <table>
+              <tr>
+                <th>Ræðumenn</th>
+                <th>Staður</th>
+                <th>Dagsetning</th>
+              </tr>
+              <tr>
+                <td>Tom Preston-Werner</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+              <tr>
+                <td>Chris Wanstrah</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+              <tr>
+                <td>PY Hyett</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+              <tr>
+                <td>Nikolaus Diodotos Schwangau</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+              <tr>
+                <td>Rameses Teodor Leitzke</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+              <tr>
+                <td>Alaric Reino Eldred</td>
+                <td>Eldborg</td>
+                <td>07.07.2017</td>
+              </tr>
+            </table>
+            <br><br><h1>Eldborg</h1>
+            <img class="eldborg" src="https://www.harpa.is/wp-content/uploads/2016/07/skodunarferdir_banner.jpg">
+					</div>    
+        </div>
+					
+					
+				<div class="back-img img4"></div>
+        <div id="i4" class="page">
+          <div class="content">
+						<h1><div class="icon-map"></div>Hvar erum við?</h1>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1739.6059651928344!2d-21.934890783499934!3d64.15033658359694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48d674d3019cb491%3A0xa813064fd5697156!2sHarpa+Concert+Hall+and+Conference+Centre!5e0!3m2!1sen!2sis!4v1478775677682" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+						<h2 class="directions">Harpan er beint við sjóninn</h2>
+						<p class="directions">Aðalhönnuðir Hörpu eru Henning Larsen Arkitektar í Kaupmannahöfn og Batteríið Arkitektar. Henning Larsen Arkitektar hafa hannað mörg sambærileg hús, þar má meðal annars nefna Óperuna í Kaupmannahöfn og Tónlistar- og ráðstefnuhúsið í Uppsölum. Ólafur Elíasson, í samvinnu við Henning Larsen Arkitekta, hannaði glerhjúpinn sem umlykur Hörpu. Eitt af megineinkennum hans er tilvísun í ýmis fyrirbæri úr náttúrunni og einstök birtuskilyrði landsins. Aðalverktaki var ÍAV..</p>
+					</div> 
+				</div>
+					
+					<footer class="footer">
+						<div>Ármann Hallbert Jónsson</div>
+						<div>Verkefni 4</div>
+						<div>Github</div>
+    				</footer>
+        </div>
+    </body>
+    <script>
+			$(document).ready(function(){
+				// Add smooth scrolling to all links
+				$("a").on('click', function(event) {
+					// Make sure this.hash has a value before overriding default behavior
+					if (this.hash !== "") {
+						// Prevent default anchor click behavior
+						event.preventDefault();
+						// Store hash
+						var hash = this.hash;
+						// Using jQuery's animate() method to add smooth page scroll
+						// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+						$('html, body').animate({
+							scrollTop: $(hash).offset().top
+						}, 1000, function(){
+							
+							// Add hash (#) to URL when done scrolling (default click behavior)
+							window.location.hash = hash;
+						});
+					} // End if
+				});
+			});
+		</script>
+</html>
